e2befe2ed27b912399660d41587587ae75336959
Evan Shabsove <evan.shabsove@swtchev.com>
2019-10-30 22:23:18 -0400

    when login now return if user has questions or now

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -5,7 +5,9 @@ class AuthenticationController < ApplicationController
    command = AuthenticateUser.call(params[:email], params[:password])
 
    if command.success?
-     render json: { auth_token: command.result, user: User.find_by_email(params[:email]) }
+     user = User.find_by_email(params[:email])
+     has_questions = user.questions.length > 0
+     render json: { auth_token: command.result, user: User.find_by_email(params[:email]), has_questions: has_questions }
    else
      render json: { error: command.errors }, status: :unauthorized
    end
